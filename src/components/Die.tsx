d6d5f5da9cf1a1ebccec585d27661ed8edf1f547
Andres <andy@mavenandy.com>
2024-03-09 17:27:34 -0600

    Updated dice component to ts

---
+++ src/components/Die.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface DiceProps {
+	id: number;
+	isHeld: boolean;
+	holdDice: () => void;
+	value: number;
+}
+
+export default function Die(props: DiceProps) {
+	return (
+		<div
+			className={`text-2xl p-3 border-slate-300 rounded-lg shadow-lg font-bold hover:bg-teal-100 hover:cursor-pointer active:opacity-75 ${
+				props.isHeld ? 'bg-teal-200' : 'bg-white'
+			}`}
+			onClick={props.holdDice}
+		>
+			<h2>{props.value}</h2>
+		</div>
+	);
+}
